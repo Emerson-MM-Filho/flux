2db963ec9ce920b3fe1106bf27724de9aa32ce4c
Emerson M M Filho <emerson.dev.machado@gmail.com>
2024-06-19 18:04:51 -0300

    refactor: create transactions state

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,13 +1,179 @@
 import BottomSheet, { BottomSheetView } from "@gorhom/bottom-sheet"
-import React, { useMemo } from "react"
+import React, { useMemo, useState } from "react"
 import { Image, ScrollView, StyleSheet, Text, View } from "react-native"
 import { SafeAreaView } from "react-native-safe-area-context"
 import AnalyticCard from "../../components/AnalyticsCard"
 import Header from "../../components/Header"
 import TransactionsList from '../../components/TransactionsList'
+import TransactionInterface from '../../interfaces/transaction'
 
 export default function HomeScreen() {
   const snapPoints = useMemo(() => ["45%", "82%"], []);
+  const [transactions, setTransactions] = useState([
+    {
+      id: 1,
+      title: "Fort Atacadista 1",
+      date: "12/03/2024",
+      value: "- R$ 15,68",
+      category: {
+        id: "1",
+        name: "Compras",
+        icon: require("@/assets/images/cart-icon.png"),
+      },
+      tags: [
+        {
+          id: 1,
+          name: "Páscoa",
+          mainColor: "#F54545",
+          backgroundColor: "rgba(245, 69, 69, 0.5)",
+        },
+        {
+          id: 2,
+          name: "Compras",
+          mainColor: "#45CBF5",
+          backgroundColor: "rgba(69, 203, 245, 0.5)",
+        },
+        {
+          id: 3,
+          name: "Compras",
+          mainColor: "#DDF545",
+          backgroundColor: "rgba(221, 245, 69, 0.5)",
+        },
+      ],
+    },
+    {
+      id: 2,
+      title: "Fort Atacadista 2",
+      date: "12/03/2024",
+      category: {
+        id: "1",
+        name: "Compras",
+        icon: require("@/assets/images/cart-icon.png"),
+      },
+      value: "- R$ 15,68",
+      tags: [],
+    },
+    {
+      id: 3,
+      title: "Fort Atacadista 3",
+      date: "12/03/2024",
+      category: {
+        id: "1",
+        name: "Compras",
+        icon: require("@/assets/images/cart-icon.png"),
+      },
+      value: "- R$ 15,68",
+      tags: [],
+    },
+    {
+      id: 4,
+      title: "Fort Atacadista 4",
+      date: "12/03/2024",
+      category: {
+        id: "1",
+        name: "Compras",
+        icon: require("@/assets/images/cart-icon.png"),
+      },
+      value: "- R$ 15,68",
+      tags: [],
+    },
+    {
+      id: 5,
+      title: "Fort Atacadista 5",
+      date: "12/03/2024",
+      category: {
+        id: "1",
+        name: "Compras",
+        icon: require("@/assets/images/cart-icon.png"),
+      },
+      value: "- R$ 15,68",
+      tags: [],
+    },
+    {
+      id: 6,
+      title: "Fort Atacadista 6",
+      date: "12/03/2024",
+      category: {
+        id: "1",
+        name: "Compras",
+        icon: require("@/assets/images/cart-icon.png"),
+      },
+      value: "- R$ 15,68",
+      tags: [],
+    },
+    {
+      id: 7,
+      title: "Fort Atacadista 7",
+      date: "12/03/2024",
+      category: {
+        id: "1",
+        name: "Compras",
+        icon: require("@/assets/images/cart-icon.png"),
+      },
+      value: "- R$ 15,68",
+      tags: [],
+    },
+    {
+      id: 8,
+      title: "Fort Atacadista 8",
+      date: "12/03/2024",
+      category: {
+        id: "1",
+        name: "Compras",
+        icon: require("@/assets/images/cart-icon.png"),
+      },
+      value: "- R$ 15,68",
+      tags: [],
+    },
+    {
+      id: 9,
+      title: "Fort Atacadista 9",
+      date: "12/03/2024",
+      category: {
+        id: "1",
+        name: "Compras",
+        icon: require("@/assets/images/cart-icon.png"),
+      },
+      value: "- R$ 15,68",
+      tags: [],
+    },
+    {
+      id: 10,
+      title: "Fort Atacadista 10",
+      date: "12/03/2024",
+      category: {
+        id: "1",
+        name: "Compras",
+        icon: require("@/assets/images/cart-icon.png"),
+      },
+      value: "- R$ 15,68",
+      tags: [],
+    },
+    {
+      id: 11,
+      title: "Fort Atacadista 11",
+      date: "12/03/2024",
+      category: {
+        id: "1",
+        name: "Compras",
+        icon: require("@/assets/images/cart-icon.png"),
+      },
+      value: "- R$ 15,68",
+      tags: [],
+    },
+    {
+      id: 12,
+      title: "Fort Atacadista 12",
+      date: "12/03/2024",
+      category: {
+        id: "1",
+        name: "Compras",
+        icon: require("@/assets/images/cart-icon.png"),
+      },
+      value: "- R$ 15,68",
+      tags: [],
+    },
+  ] as TransactionInterface[]);
 
   return (
     <SafeAreaView style={styles.tabContainer}>
@@ -80,7 +246,7 @@ export default function HomeScreen() {
             </View>
             <Text style={styles.addTransactionBtnText}>Add new</Text>
           </View>
-          <TransactionsList/>
+          <TransactionsList transactions={transactions}/>
         </BottomSheetView>
       </BottomSheet>
     </SafeAreaView>
