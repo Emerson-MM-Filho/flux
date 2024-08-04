25cb47777060bade15cba9ccb69e7307ae7ea366
Emerson Filho <emerson.dev.machado@gmail.com>
2024-08-04 04:19:13 -0300

    feature: add use keyboard avoiding view prop

--- components/ThemedView.tsx
+++ components/ThemedView.tsx
@@ -1,14 +1,22 @@
-import { View, type ViewProps } from 'react-native';
+import { View, type ViewProps, KeyboardAvoidingView } from 'react-native';
 
 import { useThemeColor } from '@/hooks/useThemeColor';
 
 export type ThemedViewProps = ViewProps & {
   lightColor?: string;
   darkColor?: string;
+  useKeyboardAvoidingView?: boolean;
 };
 
 export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
   const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
+
+  if (otherProps.useKeyboardAvoidingView) {
+    return (
+      <KeyboardAvoidingView style={[{ backgroundColor }, style]} {...otherProps} />
+    );
+  }
+
 
   return <View style={[{ backgroundColor }, style]} {...otherProps} />;
 }
